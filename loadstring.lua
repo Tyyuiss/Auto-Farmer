c347ff703a27df82043f685df8467e8cb00150ce
Tyyuiss <73793955+Tyyuiss@users.noreply.github.com>
2024-03-03 12:05:57 -0600

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,5 +1,6 @@
 _G.script_host = 'USERNAME' -- Username of the player you want the bot to teleport to!
 _G.teleport = true -- Change to False and delete the loadstring to use toggles
 _G.prefix = '/' -- / is DEFAULT prefix, /cmds for commands!
+_G.messages = true -- Whether or not it sends a message in chat!
 
 loadstring(game:HttpGet(("https://raw.githubusercontent.com/Tyyuiss/Auto-Farmer/main/code.lua"),true))()
