284dc54157f25edc827e9a88d3fdb693797eebc0
Tyyuiss <73793955+Tyyuiss@users.noreply.github.com>
2024-03-03 10:38:22 -0600

    Create loadstring.lua

---
+++ loadstring.lua
@@ -0,0 +1,4 @@
+_G.script_host = 'USERNAME' -- Username of the player you want the bot to teleport to!
+_G.teleport = true -- Change to False and delete the loadstring to use toggles
+
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/Tyyuiss/Auto-Farmer/main/code.lua"),true))()
