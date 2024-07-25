4526a923b21e828145fcd882dd0fadd0096c9a90
Tyyuiss <73793955+Tyyuiss@users.noreply.github.com>
2024-07-25 09:38:08 -0500

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,6 +1,6 @@
 _G.script_host = 'USERNAME' -- Username of the player you want the bot to teleport to! & Have commands
 _G.teleport = true -- Change to False and delete the loadstring to use toggles
-_G.prefix = '/' -- / is DEFAULT prefix, /cmds for commands!
+_G.prefix = '.' -- / is DEFAULT prefix, /cmds for commands!
 _G.messages = true -- Whether or not it sends a message in chat!
 
 _G.whitelisted_users = {'USERNAME'} -- Players that can use commands on your bot(s) other than you!
